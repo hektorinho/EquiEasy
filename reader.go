2d8311215cfc5a21b49a88bdc0aa532d012fc31c
hektorinho <rhsuhr@gmail.com>
2023-05-17 15:02:40 -0400

    removed requirement for splt times and also made it possible that first fractional wasn't 'Start'.

--- reader.go
+++ reader.go
@@ -13,19 +13,20 @@ import (
 )
 
 const (
-	REGEX_VALID_RACE_PAGE   = `.*\- [A-Za-z]+ [0-9]+\, [0-9]{4} \- Race [0-9]+`
-	REGEX_VALID_CANCELLED   = `Cancelled.*\-.*`
-	REGEX_GET_HORSES        = `(?P<datetrack>([\-]{3}\s+)|([0-9]{1}[0-9]*(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[0-9]{1}[0-9]* [A-Z]{2}[A-Z]*))(?P<pgm>(\s)*[0-9ABCX]+) (?P<horsename>[A-Za-z0-9\s\'\"\!\.\,\-\_\*\$\?]+[A-Z\(\)\w]*) \((?P<jockey>[A-Za-z0-9\,\.\s\'\-]+)\) (?P<wgt>[0-9]{3})( |.*)(?P<me>[A-Za-z\-\s]+) (?P<postposition>[0-9]{1}|[0-9]{2}) .* (?P<odds>[0-9]+\.[0-9\*]+) (?P<comment>[A-Za-z0-9\(\)\{\}\[\]\/\,\.\s\-\&\:\;\'\"\|]+)`
-	REGEX_LAST_DATE_TRACK   = `(?P<date>[0-9]{1}[0-9]*(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[0-9]{1}[0-9]*) (?P<track>[A-Z]{2}[A-Z]*)`
-	REGEX_FRACTIONALS       = `(Pg m Horse Name) (?P<fracs>Start .* Str Fin)`
-	REGEX_TOP_VALUES        = `([0-9\/\s])+|(Head)+[\s]+|(Nose)+[\s]+|(Neck)+[\s]+`
-	REGEX_HORSE_POSITIONAL  = `[0-9]{1}[0-9ABX]* (?P<horsename>[A-Za-z0-9\s\'\"\!\.\,\-\_\*\$\?]+[A-Z\(\)\w]*) [0-9]{1}[0-9]* [0-9]{1}[0-9]* [0-9]{1}[0-9]*.*`
-	REGEX_HORSE_TABLE_INIT  = `[0-9]{1}[0-9ABX]* (?P<horsename>[A-Za-z0-9\s\'\"\!\.\,\-\_\*\$\?]+[A-Z\(\)\w]*) (?P<start>[0-9]{1}[0-9]*) (?P<firstfrac>[0-9]{1}[0-9]*)`
-	REGEX_TRAINERS          = `Trainers:(?P<trainers>.*)`
-	REGEX_TRAINERS_STOP     = `Owners:.*`
-	REGEX_OWNERS            = `Owners:(?P<owners>.*)`
-	REGEX_OWNERS_STOP       = `Footnotes.*`
-	REGEX_IND_TRAINER_OWNER = `(?P<number>[0-9]{1}[0-9abxABX]*)(?P<sep>(\s|\-)(\s|\-)*(\s)*)(?P<name>[A-Za-z0-9\,\.\s\'\-\(\)]+)`
+	REGEX_VALID_RACE_PAGE      = `.*\- [A-Za-z]+ [0-9]+\, [0-9]{4} \- Race [0-9]+`
+	REGEX_VALID_CANCELLED      = `Cancelled.*\-.*`
+	REGEX_GET_HORSES           = `(?P<datetrack>([\-]{3}\s+)|([0-9]{1}[0-9]*(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[0-9]{1}[0-9]* [A-Z]{2}[A-Z]*))(?P<pgm>(\s)*[0-9ABCX]+) (?P<horsename>[A-Za-z0-9\s\'\"\!\.\,\-\_\*\$\?]+[A-Z\(\)\w]*) \((?P<jockey>[A-Za-z0-9\,\.\s\'\-]+)\) (?P<wgt>[0-9]{3})( |.*)(?P<me>[A-Za-z\-\s]+) (?P<postposition>[0-9]{1}|[0-9]{2}) .* (?P<odds>[0-9]+\.[0-9\*]+) (?P<comment>[A-Za-z0-9\(\)\{\}\[\]\/\,\.\s\-\&\:\;\'\"\|]+)`
+	REGEX_LAST_DATE_TRACK      = `(?P<date>[0-9]{1}[0-9]*(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[0-9]{1}[0-9]*) (?P<track>[A-Z]{2}[A-Z]*)`
+	REGEX_FRACTIONALS          = `(Pg m Horse Name) (?P<fracs>(Start|1\/4|3\/8|1\/2) .* Str Fin)`
+	REGEX_TOP_VALUES           = `([0-9\/\s])+|(Head)+[\s]+|(Nose)+[\s]+|(Neck)+[\s]+`
+	REGEX_HORSE_POSITIONAL     = `[0-9]{1}[0-9ABX]* (?P<horsename>[A-Za-z0-9\s\'\"\!\.\,\-\_\*\$\?]+[A-Z\(\)\w]*) [0-9]{1}[0-9]* [0-9]{1}[0-9]* [0-9]{1}[0-9]*.*`
+	REGEX_HORSE_TABLE_W_START  = `[0-9]{1}[0-9ABX]* (?P<horsename>[A-Za-z0-9\s\'\"\!\.\,\-\_\*\$\?]+[A-Z\(\)\w]*) (?P<start>[0-9]{1}[0-9]*) (?P<firstfrac>[0-9]{1}[0-9]*)`
+	REGEX_HORSE_TABLE_WO_START = `[0-9]{1}[0-9ABX]* (?P<horsename>[A-Za-z0-9\s\'\"\!\.\,\-\_\*\$\?]+[A-Z\(\)\w]*) (?P<firstfrac>[0-9]{1}[0-9]*)`
+	REGEX_TRAINERS             = `Trainers:(?P<trainers>.*)`
+	REGEX_TRAINERS_STOP        = `Owners:.*`
+	REGEX_OWNERS               = `Owners:(?P<owners>.*)`
+	REGEX_OWNERS_STOP          = `Footnotes.*`
+	REGEX_IND_TRAINER_OWNER    = `(?P<number>[0-9]{1}[0-9abxABX]*)(?P<sep>(\s|\-)(\s|\-)*(\s)*)(?P<name>[A-Za-z0-9\,\.\s\'\-\(\)]+)`
 
 	REGEX_RACE_TRACK                = `[ \*\[\]\/\\\\?\<\>]`
 	REGEX_RACE_NUMBER               = `Race (?P<number>[0-9]+)`
@@ -69,20 +70,21 @@ type RaceMetadata struct {
 }
 
 type Horse struct {
-	RaceHash     string
-	Name         string
-	Number       string
-	PostPosition string
-	Weight       string
-	MedEquipment string
-	Jockey       string
-	Trainer      string
-	Owners       string
-	Odds         string
-	Comments     string
-	LastRaced    time.Time
-	LastTrack    string
-	Fractionals  []Fractional
+	RaceHash      string
+	Name          string
+	Number        string
+	PostPosition  string
+	Weight        string
+	MedEquipment  string
+	Jockey        string
+	Trainer       string
+	Owners        string
+	Odds          string
+	Comments      string
+	LastRaced     time.Time
+	LastTrack     string
+	Fractionals   []Fractional
+	withTopOffset bool
 }
 
 type Fractional struct {
@@ -96,8 +98,138 @@ type ValidPages struct {
 	Pages    []pdf.Page
 }
 
+// Returns all valid race pages in an Equibase Document.
+func GetValidPages(f string) (ValidPages, error) {
+	racepages := []pdf.Page{}
+	r, err := pdf.Open(f)
+	if err != nil {
+		return ValidPages{}, err
+	}
+
+	for i := 1; i <= r.NumPage(); i++ {
+		page := r.Page(i)
+		if IsValidRacePage(page) {
+			racepages = append(racepages, page)
+		}
+	}
+	return ValidPages{Filename: f, Pages: racepages}, nil
+}
+
+// Generates a new RacePage including all horses and metadata about the race.
+func NewRacePage(page pdf.Page) (*RacePage, error) {
+	meta, err := Metadata(page)
+	if err != nil {
+		return nil, err
+	}
+	horses, err := Horses(page)
+	if err != nil {
+		return nil, err
+	}
+	return &RacePage{Metadata: meta, Horses: horses}, nil
+}
+
+// Create a new metadata struct from a page
+func Metadata(page pdf.Page) (*RaceMetadata, error) {
+	re := regexp.MustCompile(REGEX_RACE_TRACK)
+	renum := regexp.MustCompile(REGEX_RACE_NUMBER)
+	r := &RaceMetadata{}
+	var racehash string
+
+	rows, err := page.GetTextByRow()
+	if err != nil {
+		return nil, err
+	}
+
+	for i, row := range rows {
+		rowdata := []byte{}
+		if row.Position == 760 {
+			for _, word := range row.Content {
+				rowdata = append(rowdata, word.S...)
+				rowdata = append(rowdata, " "...)
+			}
+			if i == 0 {
+				racehash = fmt.Sprintf("%x", md5.Sum(rowdata))
+			}
+
+			data := bytes.Split(rowdata, []byte(" - "))
+
+			if len(data) != 3 {
+				return nil, fmt.Errorf("wasn't able to gather track and date data from race")
+			}
+
+			r.Track = string(re.ReplaceAll(data[0], []byte("")))
+			date, err := time.Parse("January 2, 2006", string(data[1]))
+			if err != nil {
+				return nil, err
+			}
+			r.Date = date
+			r.Number, err = strconv.Atoi(string(bytes.TrimSpace(renum.ReplaceAll(data[2], []byte("$number")))))
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	horsetype, err := r.genericDataFromRegex(page, REGEX_RACE_HORSETYPE)
+	if err != nil {
+		return nil, err
+	}
+	racetype, err := r.genericDataFromRegex(page, REGEX_RACE_RACETYPE)
+	if err != nil {
+		return nil, err
+	}
+	purse, err := r.genericDataFromRegex(page, REGEX_RACE_PURSE)
+	if err != nil {
+		return nil, err
+	}
+	weather, err := r.genericDataFromRegex(page, REGEX_RACE_WEATHER)
+	if err != nil {
+		return nil, err
+	}
+	trackcondition, err := r.genericDataFromRegex(page, REGEX_RACE_TRACK_CONDITION)
+	if err != nil {
+		return nil, err
+	}
+	length, err := r.genericDataFromRegex(page, REGEX_RACE_LENGTH)
+	if err != nil {
+		return nil, err
+	}
+	trackrecord, err := r.genericDataFromRegex(page, REGEX_RACE_CURRENT_TRACK_RECORD)
+	if err != nil {
+		return nil, err
+	}
+	finaltime, err := r.genericDataFromRegex(page, REGEX_RACE_FINAL_TIME)
+	if err != nil {
+		return nil, err
+	}
+	fractionaltimes, err := r.genericDataFromRegex(page, REGEX_RACE_FRACTIONAL_TIMES)
+	if err != nil {
+		return nil, err
+	}
+	splittimes, err := r.genericDataFromRegex(page, REGEX_RACE_SPLIT_TIMES)
+	if err != nil {
+		return nil, err
+	}
+	runup, err := r.genericDataFromRegex(page, REGEX_RACE_RUN_UP)
+	if err != nil {
+		return nil, err
+	}
+	r.RaceHash = string(racehash)
+	r.HorseType = string(horsetype)
+	r.Type = string(racetype)
+	r.Purse = string(purse)
+	r.Weather = string(weather)
+	r.TrackCondition = string(trackcondition)
+	r.RaceLength = string(length)
+	r.CurrentTrackRecord = string(trackrecord)
+	r.FinalTime = string(finaltime)
+	r.FractionalTimes = string(fractionaltimes)
+	r.SplitTimes = string(splittimes)
+	r.RunUp = string(runup)
+	return r, nil
+}
+
 // Get a list of all Horses racing on a race page.
-func GetHorses(page pdf.Page) ([]*Horse, error) {
+func Horses(page pdf.Page) ([]*Horse, error) {
 	horses := []*Horse{}
 	re := regexp.MustCompile(REGEX_GET_HORSES)
 	re2 := regexp.MustCompile(REGEX_LAST_DATE_TRACK)
@@ -146,28 +278,12 @@ func GetHorses(page pdf.Page) ([]*Horse, error) {
 	return horses, nil
 }
 
-// Returns all valid race pages in an Equibase Document.
-
-// BIG TODO: if race is cancelled it will still have a header but second row will say Cancelled due to Weather!!!
-// SEE BEL102718USA.pdf as and example!!!
-func GetValidPages(f string) (ValidPages, error) {
-	racepages := []pdf.Page{}
-	r, err := pdf.Open(f)
-	if err != nil {
-		return ValidPages{}, err
-	}
-
-	for i := 1; i <= r.NumPage(); i++ {
-		page := r.Page(i)
-		if IsValidRacePage(page) {
-			racepages = append(racepages, page)
-		}
-	}
-	return ValidPages{Filename: f, Pages: racepages}, nil
-}
+// TODO:
+// Scans a wider margin for DOH (Distance to Other Horses) values rather than just the expected spot.
+func SetTopOffsetOn() {}
 
 // Returns a list of the positioned fractionals for a race.
-func Fractionals(page pdf.Page) ([]string, int64, error) {
+func fractionals(page pdf.Page) ([]string, int64, error) {
 	re := regexp.MustCompile(REGEX_FRACTIONALS)
 	rows, err := page.GetTextByRow()
 	if err != nil {
@@ -180,7 +296,6 @@ func Fractionals(page pdf.Page) ([]string, int64, error) {
 			rowdata = append(rowdata, word.S...)
 			rowdata = append(rowdata, " "...)
 		}
-
 		if re.Match(rowdata) {
 			fractionals := strings.Split(string(re.ReplaceAll(bytes.Trim(rowdata, " "), []byte("$fracs"))), " ")
 			return fractionals, row.Position, nil
@@ -189,8 +304,8 @@ func Fractionals(page pdf.Page) ([]string, int64, error) {
 	return nil, -99, fmt.Errorf("failed to gather any fractional data from page")
 }
 
-func (h *Horse) ApplyFractionalData(page pdf.Page) error {
-	frs, y, err := Fractionals(page)
+func (h *Horse) applyFractionalData(page pdf.Page) error {
+	frs, y, err := fractionals(page)
 	if err != nil {
 		return err
 	}
@@ -200,7 +315,8 @@ func (h *Horse) ApplyFractionalData(page pdf.Page) error {
 		return err
 	}
 
-	re := regexp.MustCompile(REGEX_HORSE_TABLE_INIT)
+	re := regexp.MustCompile(REGEX_HORSE_TABLE_W_START)
+	re2 := regexp.MustCompile(REGEX_HORSE_TABLE_WO_START)
 
 	fractionals := []Fractional{}
 	listoftop := [][]float64{}
@@ -225,25 +341,39 @@ func (h *Horse) ApplyFractionalData(page pdf.Page) error {
 				horseString = fmt.Sprintf("%s %s", h.Number, h.Name)
 			}
 
-			if horseString == string(rowdata[:len(horseString)]) {
-				values := bytes.Split(rowdata, []byte(" | "))
-				for _, val := range values {
-					if re.Match(val) {
-						start := Fractional{Position: strings.Trim(string(re.ReplaceAll(val, []byte("$start"))), " ")}
-						firstfrac := Fractional{Position: strings.Trim(string(re.ReplaceAll(val, []byte("$firstfrac"))), " ")}
-						fractionals = append(fractionals, start, firstfrac)
-					} else {
-						frac := Fractional{Position: strings.Trim(string(val), " ")}
-						fractionals = append(fractionals, frac)
-					}
-				}
+			if len(rowdata) >= len(horseString) {
+				if horseString == string(rowdata[:len(horseString)]) {
+					values := bytes.Split(rowdata, []byte(" | "))
+					for _, val := range values {
+						switch frs[0] {
+						case "Start":
+							if re.Match(val) {
+								start := Fractional{Position: strings.Trim(string(re.ReplaceAll(val, []byte("$start"))), " ")}
+								firstfrac := Fractional{Position: strings.Trim(string(re.ReplaceAll(val, []byte("$firstfrac"))), " ")}
+								fractionals = append(fractionals, start, firstfrac)
+							} else {
+								frac := Fractional{Position: strings.Trim(string(val), " ")}
+								fractionals = append(fractionals, frac)
+							}
+						default:
+							if re2.Match(val) {
+								firstfrac := Fractional{Position: strings.Trim(string(re.ReplaceAll(val, []byte("$firstfrac"))), " ")}
+								fractionals = append(fractionals, firstfrac)
+							} else {
+								frac := Fractional{Position: strings.Trim(string(val), " ")}
+								fractionals = append(fractionals, frac)
+							}
 
-				prevX = 0
-				for _, word := range row.Content {
-					if word.X > prevX {
-						listoftop = append(listoftop, []float64{roundFloat(word.X+OFFSET_TOP_X, 3), roundFloat(word.Y+OFFSET_TOP_Y, 3)})
+						}
 					}
-					prevX = word.X
+
+					prevX = 0
+					for _, word := range row.Content {
+						if word.X > prevX {
+							listoftop = append(listoftop, []float64{roundFloat(word.X+OFFSET_TOP_X, 3), roundFloat(word.Y+OFFSET_TOP_Y, 3)})
+						}
+						prevX = word.X
+					}
 				}
 			}
 		}
@@ -251,16 +381,25 @@ func (h *Horse) ApplyFractionalData(page pdf.Page) error {
 
 	listoftop[0][0] = roundFloat(listoftop[1][0]-(listoftop[2][0]-listoftop[1][0]), 3)
 
+	offset := len(fractionals) - len(listoftop)
+
+	// TODO:
+	// When function in place, set default to 0.0
+	offset_from_word := 4.0
+	if h.withTopOffset {
+		offset_from_word = 4.0
+	}
+
 	for i, item := range listoftop {
 		for _, row := range rows {
 			prevX := 0.0
 			for _, word := range row.Content {
-				if (word.X == item[0]) && (word.Y == item[1]) {
+				if (word.X >= item[0]-offset_from_word) && (word.X <= item[0]+offset_from_word) && (word.Y == item[1]) {
 					if prevX == word.X {
-						fractionals[i+1].Lengths += " " + word.S
+						fractionals[i+offset].Lengths += " " + word.S
 					}
 					if prevX != word.X {
-						fractionals[i+1].Lengths = word.S
+						fractionals[i+offset].Lengths = word.S
 					}
 				}
 				prevX = word.X
@@ -276,7 +415,7 @@ func (h *Horse) ApplyFractionalData(page pdf.Page) error {
 }
 
 // Scans the document for Trainers and applies them to the horse
-func (h *Horse) ApplyTrainerData(page pdf.Page) error {
+func (h *Horse) applyTrainerData(page pdf.Page) error {
 	re := regexp.MustCompile(REGEX_TRAINERS)
 	retr := regexp.MustCompile(REGEX_IND_TRAINER_OWNER)
 	restop := regexp.MustCompile(REGEX_TRAINERS_STOP)
@@ -322,7 +461,7 @@ func (h *Horse) ApplyTrainerData(page pdf.Page) error {
 }
 
 // Scans the document for Owners and applies them to the horse
-func (h *Horse) ApplyOwnerData(page pdf.Page) error {
+func (h *Horse) applyOwnerData(page pdf.Page) error {
 	re := regexp.MustCompile(REGEX_OWNERS)
 	retr := regexp.MustCompile(REGEX_IND_TRAINER_OWNER)
 	restop := regexp.MustCompile(REGEX_OWNERS_STOP)
@@ -368,108 +507,8 @@ func (h *Horse) ApplyOwnerData(page pdf.Page) error {
 	return nil
 }
 
-// Create a new metadata struct from a page
-func NewMetadata(page pdf.Page) (*RaceMetadata, error) {
-	re := regexp.MustCompile(REGEX_RACE_TRACK)
-	renum := regexp.MustCompile(REGEX_RACE_NUMBER)
-	r := &RaceMetadata{}
-	var racehash string
-
-	rows, err := page.GetTextByRow()
-	if err != nil {
-		return nil, err
-	}
-
-	for i, row := range rows {
-		rowdata := []byte{}
-		if row.Position == 760 {
-			for _, word := range row.Content {
-				rowdata = append(rowdata, word.S...)
-				rowdata = append(rowdata, " "...)
-			}
-			if i == 0 {
-				racehash = fmt.Sprintf("%x", md5.Sum(rowdata))
-			}
-
-			data := bytes.Split(rowdata, []byte(" - "))
-
-			if len(data) != 3 {
-				return nil, fmt.Errorf("wasn't able to gather track and date data from race")
-			}
-
-			r.Track = string(re.ReplaceAll(data[0], []byte("")))
-			date, err := time.Parse("January 2, 2006", string(data[1]))
-			if err != nil {
-				return nil, err
-			}
-			r.Date = date
-			r.Number, err = strconv.Atoi(string(bytes.TrimSpace(renum.ReplaceAll(data[2], []byte("$number")))))
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-	horsetype, err := r.GenericDataFromRegex(page, REGEX_RACE_HORSETYPE)
-	if err != nil {
-		return nil, err
-	}
-	racetype, err := r.GenericDataFromRegex(page, REGEX_RACE_RACETYPE)
-	if err != nil {
-		return nil, err
-	}
-	purse, err := r.GenericDataFromRegex(page, REGEX_RACE_PURSE)
-	if err != nil {
-		return nil, err
-	}
-	weather, err := r.GenericDataFromRegex(page, REGEX_RACE_WEATHER)
-	if err != nil {
-		return nil, err
-	}
-	trackcondition, err := r.GenericDataFromRegex(page, REGEX_RACE_TRACK_CONDITION)
-	if err != nil {
-		return nil, err
-	}
-	length, err := r.GenericDataFromRegex(page, REGEX_RACE_LENGTH)
-	if err != nil {
-		return nil, err
-	}
-	trackrecord, err := r.GenericDataFromRegex(page, REGEX_RACE_CURRENT_TRACK_RECORD)
-	if err != nil {
-		return nil, err
-	}
-	finaltime, err := r.GenericDataFromRegex(page, REGEX_RACE_FINAL_TIME)
-	if err != nil {
-		return nil, err
-	}
-	fractionaltimes, err := r.GenericDataFromRegex(page, REGEX_RACE_FRACTIONAL_TIMES)
-	if err != nil {
-		return nil, err
-	}
-	splittimes, err := r.GenericDataFromRegex(page, REGEX_RACE_SPLIT_TIMES)
-	if err != nil {
-		return nil, err
-	}
-	runup, err := r.GenericDataFromRegex(page, REGEX_RACE_RUN_UP)
-	if err != nil {
-		return nil, err
-	}
-	r.RaceHash = string(racehash)
-	r.HorseType = string(horsetype)
-	r.Type = string(racetype)
-	r.Purse = string(purse)
-	r.Weather = string(weather)
-	r.TrackCondition = string(trackcondition)
-	r.RaceLength = string(length)
-	r.CurrentTrackRecord = string(trackrecord)
-	r.FinalTime = string(finaltime)
-	r.FractionalTimes = string(fractionaltimes)
-	r.SplitTimes = string(splittimes)
-	r.RunUp = string(runup)
-	return r, nil
-}
-
 // Gather generic data from page from a regex expression.
-func (r *RaceMetadata) GenericDataFromRegex(page pdf.Page, regex string) ([]byte, error) {
+func (r *RaceMetadata) genericDataFromRegex(page pdf.Page, regex string) ([]byte, error) {
 	re := regexp.MustCompile(regex)
 
 	rows, err := page.GetTextByRow()
@@ -489,17 +528,8 @@ func (r *RaceMetadata) GenericDataFromRegex(page pdf.Page, regex string) ([]byte
 			return data, nil
 		}
 	}
+	if regex == REGEX_RACE_SPLIT_TIMES {
+		return nil, nil
+	}
 	return nil, fmt.Errorf("wasn't able to grab the generic data requested\nregex >> %s", regex)
-}
-
-func NewRacePage(page pdf.Page) (*RacePage, error) {
-	meta, err := NewMetadata(page)
-	if err != nil {
-		return nil, err
-	}
-	horses, err := GetHorses(page)
-	if err != nil {
-		return nil, err
-	}
-	return &RacePage{Metadata: meta, Horses: horses}, nil
 }
--- reader_test.go
+++ reader_test.go
@@ -7,16 +7,20 @@ import (
 
 const (
 	// testFile = "data/eqbPDFChartPlus.pdf"
-	testFile = "C:/GoProjects/Scratch/Test/collected/BEL050910USA.pdf"
+
+	// testFile = "C:/GoProjects/Scratch/Test/collected/BEL050910USA.pdf"
+	// testFile = "C:/GoProjects/Scratch/Test/collected/BEL051001USA2.pdf"
+	testFile = "C:/GoProjects/Scratch/Test/collected/BEL051118USA.pdf"
 )
 
-func TestGetHorses(t *testing.T) {
+func TestHorses(t *testing.T) {
 	pages, err := GetValidPages(testFile)
 	if err != nil {
 		t.Errorf("wasnt able to get valid pages from %s\nerror >>> %s\n", testFile, err)
 	}
 	for i, page := range pages.Pages {
-		if horses, err := GetHorses(page); err != nil {
+		// if i == 1 {
+		if horses, err := Horses(page); err != nil {
 			if len(horses) > 3 {
 				t.Logf("Horse 1: %s, Horse 2: %s, Horse 3: %s\n", horses[0].Name, horses[1].Name, horses[2].Name)
 			} else {
@@ -26,6 +30,7 @@ func TestGetHorses(t *testing.T) {
 				t.Errorf("expected more than one horse per race, got %d\n", len(horses))
 			}
 		}
+		// }
 	}
 }
 
@@ -65,14 +70,16 @@ func TestFractionals(t *testing.T) {
 	if err != nil {
 		t.Errorf("wasnt able to get valid pages from %s\nerror >>> %s\n", testFile, err)
 	}
-	for _, page := range pages.Pages {
-		fracs, _, err := Fractionals(page)
+	for i, page := range pages.Pages {
+		// if i == 3 {
+		fracs, _, err := fractionals(page)
 		if err != nil {
-			t.Errorf("error >>> %s\n", err)
+			t.Errorf("page >> %d || error >>> %s\n", i+1, err)
 		}
 		if len(fracs) < 1 {
 			t.Errorf("expected to get more than 0 fracs, got %d\n", len(fracs))
 		}
+		// }
 	}
 }
 
@@ -83,13 +90,13 @@ func TestApplyFractionals(t *testing.T) {
 	}
 
 	for i, page := range pages.Pages {
-		// if i == 5 {
-		horses, err := GetHorses(page)
+		// if i == 3 {
+		horses, err := Horses(page)
 		if err != nil {
 			t.Errorf("wasnt able to get horse data for page %d\n", i+1)
 		}
 		for _, horse := range horses {
-			if err := horse.ApplyFractionalData(page); err != nil {
+			if err := horse.applyFractionalData(page); err != nil {
 				t.Errorf("wasnt able to asssemble fractional data points for page %d and horse %s\n", i+1, horse.Name)
 			}
 			if len(horse.Fractionals) < 1 {
@@ -107,16 +114,16 @@ func TestApplyTrainerData(t *testing.T) {
 	}
 
 	for i, page := range pages.Pages {
-		// if i == 5 {
-		horses, err := GetHorses(page)
+		// if i == 1 {
+		horses, err := Horses(page)
 		if err != nil {
 			t.Errorf("wasnt able to get horse data for page %d\n", i+1)
 		}
 		for _, horse := range horses {
-			if err := horse.ApplyFractionalData(page); err != nil {
+			if err := horse.applyFractionalData(page); err != nil {
 				t.Errorf("wasnt able to asssemble fractional data points for page %d and horse %s\n", i+1, horse.Name)
 			}
-			if err := horse.ApplyTrainerData(page); err != nil {
+			if err := horse.applyTrainerData(page); err != nil {
 				t.Errorf("wasnt able to asssemble trainer for page %d and horse %s\n", i+1, horse.Name)
 			}
 			if len(horse.Trainer) < 1 {
@@ -134,16 +141,16 @@ func TestApplyOwnerData(t *testing.T) {
 	}
 
 	for i, page := range pages.Pages {
-		// if i == 5 {
-		horses, err := GetHorses(page)
+		// if i == 1 {
+		horses, err := Horses(page)
 		if err != nil {
 			t.Errorf("wasnt able to get horse data for page %d\n", i+1)
 		}
 		for _, horse := range horses {
-			if err := horse.ApplyFractionalData(page); err != nil {
+			if err := horse.applyFractionalData(page); err != nil {
 				t.Errorf("wasnt able to asssemble fractional data points for page %d and horse %s\n", i+1, horse.Name)
 			}
-			if err := horse.ApplyOwnerData(page); err != nil {
+			if err := horse.applyOwnerData(page); err != nil {
 				t.Errorf("wasnt able to asssemble trainer for page %d and horse %s\n", i+1, horse.Name)
 			}
 			if len(horse.Owners) < 1 {
@@ -161,7 +168,8 @@ func TestTrackAndDate(t *testing.T) {
 	}
 
 	for i, page := range pages.Pages {
-		race, err := NewMetadata(page)
+		// if i == 5 {
+		race, err := Metadata(page)
 		if err != nil {
 			t.Errorf("wasn't able to gather track and date data from page %d\nerror >> %s\n", i+1, err)
 		}
@@ -174,6 +182,7 @@ func TestTrackAndDate(t *testing.T) {
 		if race.Number == 0 {
 			t.Errorf("wasn't gather the race number. got number=%d", race.Number)
 		}
+		// }
 	}
 }
 
@@ -186,38 +195,40 @@ func TestGenericDataFromRegex(t *testing.T) {
 	for i, page := range pages.Pages {
 		// if i == 8 {
 		meta := RaceMetadata{}
-		if horsetype, err := meta.GenericDataFromRegex(page, REGEX_RACE_HORSETYPE); err != nil || horsetype == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, horsetype, err)
+		if horsetype, err := meta.genericDataFromRegex(page, REGEX_RACE_HORSETYPE); err != nil || horsetype == nil {
+			t.Errorf("wasn't able to gather horse type data from page %d, %s\nerror >> %s", i+1, horsetype, err)
 		}
-		if racetype, err := meta.GenericDataFromRegex(page, REGEX_RACE_RACETYPE); err != nil || racetype == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, racetype, err)
+		if racetype, err := meta.genericDataFromRegex(page, REGEX_RACE_RACETYPE); err != nil || racetype == nil {
+			t.Errorf("wasn't able to gather RACETYPE data from page %d, %s\nerror >> %s", i+1, racetype, err)
 		}
-		if purse, err := meta.GenericDataFromRegex(page, REGEX_RACE_PURSE); err != nil || purse == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, purse, err)
+		if purse, err := meta.genericDataFromRegex(page, REGEX_RACE_PURSE); err != nil || purse == nil {
+			t.Errorf("wasn't able to gather PURSE data from page %d, %s\nerror >> %s", i+1, purse, err)
 		}
-		if weather, err := meta.GenericDataFromRegex(page, REGEX_RACE_WEATHER); err != nil || weather == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, weather, err)
+		if weather, err := meta.genericDataFromRegex(page, REGEX_RACE_WEATHER); err != nil || weather == nil {
+			t.Errorf("wasn't able to gather WEATHER data from page %d, %s\nerror >> %s", i+1, weather, err)
 		}
-		if trackcondition, err := meta.GenericDataFromRegex(page, REGEX_RACE_TRACK_CONDITION); err != nil || trackcondition == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, trackcondition, err)
+		if trackcondition, err := meta.genericDataFromRegex(page, REGEX_RACE_TRACK_CONDITION); err != nil || trackcondition == nil {
+			t.Errorf("wasn't able to gather TRACK_CONDITION data from page %d, %s\nerror >> %s", i+1, trackcondition, err)
 		}
-		if length, err := meta.GenericDataFromRegex(page, REGEX_RACE_LENGTH); err != nil || length == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, length, err)
+		if length, err := meta.genericDataFromRegex(page, REGEX_RACE_LENGTH); err != nil || length == nil {
+			t.Errorf("wasn't able to gather RACE_LENGTH data from page %d, %s\nerror >> %s", i+1, length, err)
 		}
-		if trackrecord, err := meta.GenericDataFromRegex(page, REGEX_RACE_CURRENT_TRACK_RECORD); err != nil || trackrecord == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, trackrecord, err)
+		if trackrecord, err := meta.genericDataFromRegex(page, REGEX_RACE_CURRENT_TRACK_RECORD); err != nil || trackrecord == nil {
+			t.Errorf("wasn't able to gather CURRENT_TRACK_RECORD data from page %d, %s\nerror >> %s", i+1, trackrecord, err)
 		}
-		if finaltime, err := meta.GenericDataFromRegex(page, REGEX_RACE_FINAL_TIME); err != nil || finaltime == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, finaltime, err)
+		if finaltime, err := meta.genericDataFromRegex(page, REGEX_RACE_FINAL_TIME); err != nil || finaltime == nil {
+			t.Errorf("wasn't able to gather RACE_FINAL_TIME data from page %d, %s\nerror >> %s", i+1, finaltime, err)
 		}
-		if fractionaltimes, err := meta.GenericDataFromRegex(page, REGEX_RACE_FRACTIONAL_TIMES); err != nil || fractionaltimes == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, fractionaltimes, err)
+		if fractionaltimes, err := meta.genericDataFromRegex(page, REGEX_RACE_FRACTIONAL_TIMES); err != nil || fractionaltimes == nil {
+			t.Errorf("wasn't able to gather FRACTIONAL_TIMES data from page %d, %s\nerror >> %s", i+1, fractionaltimes, err)
 		}
-		if splittimes, err := meta.GenericDataFromRegex(page, REGEX_RACE_SPLIT_TIMES); err != nil || splittimes == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, splittimes, err)
-		}
-		if runup, err := meta.GenericDataFromRegex(page, REGEX_RACE_RUN_UP); err != nil || runup == nil {
-			t.Errorf("wasn't able to gather generic data from page %d, %s\nerror >> %s", i+1, runup, err)
+
+		// Split times can sometimes be nil if clock malfunction during race ...
+		// if splittimes, err := meta.genericDataFromRegex(page, REGEX_RACE_SPLIT_TIMES); err != nil || splittimes == nil {
+		// 	t.Errorf("wasn't able to gather SPLIT_TIMES data from page %d, %s\nerror >> %s", i+1, splittimes, err)
+		// }
+		if runup, err := meta.genericDataFromRegex(page, REGEX_RACE_RUN_UP); err != nil || runup == nil {
+			t.Errorf("wasn't able to gather RUN_UP data from page %d, %s\nerror >> %s", i+1, runup, err)
 		}
 		// }
 	}
